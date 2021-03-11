9794837cca742b17d01b626e361b4cc881babf28
praveen005 <praveen08h@gmail.com>
2021-03-11 07:55:10 +0530

    Added a line

--- git_intro/file01.js
+++ git_intro/file01.js
@@ -1,6 +1,7 @@
 function greet(name){
     console.log('Hello' + name)
-    console.log('Knock Knock '+ name)
+    console.log('Knock Knock ' + name)
+    console.log('Knock Knock knock ' + name)
 }
 
 greet('Prateek')
